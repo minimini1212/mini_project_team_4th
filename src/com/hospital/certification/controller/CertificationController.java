4f5a14862cb7f267033f553f2542a30a5443a460
erlnrln <jdw5144@naver.com>
2025-05-05 06:50:04 +0900

    fix: 코드수정

--- src/com/hospital/certification/controller/CertificationController.java
+++ src/com/hospital/certification/controller/CertificationController.java
@@ -14,18 +14,35 @@ public class CertificationController {
     private final EmployeeCertificationView ecView = new EmployeeCertificationView();
 
     public void start() {
-        while(true) {
+        while (true) {
             int menu = certView.showMainMenu();
-            switch(menu) {
-                case 1 -> certView.showList(certSvc.getAll());
-                case 2 -> certSvc.create(certView.inputCertification());
-                case 3 -> certSvc.modify(certView.inputCertificationForUpdate());
-                case 4 -> certSvc.remove(certView.inputCertId());
-                case 5 -> ecView.showList(ecSvc.getByEmployee(ecView.inputEmployeeId()));
-                case 6 -> ecSvc.addCertification(ecView.inputEmployeeCertification());
-                case 7 -> ecSvc.removeCertification(ecView.inputEmpCertId());
-                case 0 -> { return; }
-                default -> certView.showError("잘못된 선택입니다.");
+            switch (menu) {
+                case 1:
+                    certView.showList(certSvc.getAll());
+                    break;
+                case 2:
+                    certSvc.create(certView.inputCertification());
+                    break;
+                case 3:
+                    certSvc.modify(certView.inputCertificationForUpdate());
+                    break;
+                case 4:
+                    certSvc.remove(certView.inputCertId());
+                    break;
+                case 5:
+                    ecView.showList(ecSvc.getByEmployee(ecView.inputEmployeeId()));
+                    break;
+                case 6:
+                    ecSvc.addCertification(ecView.inputEmployeeCertification());
+                    break;
+                case 7:
+                    ecSvc.removeCertification(ecView.inputEmpCertId());
+                    break;
+                case 0:
+                    return;
+                default:
+                    certView.showError("잘못된 선택입니다.");
+                    break;
             }
         }
     }
