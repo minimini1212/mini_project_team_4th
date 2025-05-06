a79d89b6e7dd89a50c137393fbeeba5b0eb9e989
Gorani77 <sp2877@naver.com>
2025-05-06 19:00:27 +0900

    refactor : VIEW 클래스 전체적인 UI 변경

--- src/humanResource/common/view/HumanResourceView.java
+++ src/humanResource/common/view/HumanResourceView.java
@@ -1,12 +1,16 @@
 package humanResource.common.view;
 
 public class HumanResourceView {
-    public void humanResourceMenu(){
-//        System.out.println("\n전체메뉴 > 인사관리");
-        System.out.println("\n=== 인사 관리 부서===");
-        System.out.println("0️⃣ 뒤로 가기");
+    public void humanResourceMenu() {
+        System.out.println();
+        System.out.println("  ▌│█║▌║▌       𝙃   𝙍      ▌║▌║█│▌");
+        System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+        System.out.println();
+        System.out.println("0️⃣ 이전 메뉴 돌아가기");
         System.out.println("1️⃣ 직원 관리");
         System.out.println("2️⃣ 자격증 관리");
         System.out.println("3️⃣ 휴가 관리");
+        System.out.println();
+        System.out.print("⏩ ");
     }
 }
