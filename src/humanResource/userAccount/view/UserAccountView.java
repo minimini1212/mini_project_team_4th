87c16f3671fd8201ba6d319ff89b0dfd196ff305
keemzleun <jin9ni0120@naver.com>
2025-05-06 15:15:35 +0900

    fix: menu UI 수정

--- src/humanResource/userAccount/view/UserAccountView.java
+++ src/humanResource/userAccount/view/UserAccountView.java
@@ -2,10 +2,11 @@ package humanResource.userAccount.view;
 
 public class UserAccountView {
     public void findUserAccountMenu(){
+
         System.out.println("\n====== 병원 관리 시스템 ======");
-        System.out.println("0. 종료");
-        System.out.println("1. 로그인");
-        System.out.println("2. 회원가입");
-        System.out.print("선택: ");
+        System.out.println("0️⃣ 종료");
+        System.out.println("1️⃣ 로그인");
+        System.out.println("2️⃣ 회원가입");
+        System.out.print("\u23E9 ");
     }
 }
