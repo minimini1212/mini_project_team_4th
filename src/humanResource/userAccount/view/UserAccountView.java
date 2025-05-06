85823db28ef14b596dbca8e253d0d85e41223caa
Gorani77 <sp2877@naver.com>
2025-05-06 15:29:54 +0900

    faet : 메인메뉴 수정

--- src/humanResource/userAccount/view/UserAccountView.java
+++ src/humanResource/userAccount/view/UserAccountView.java
@@ -4,8 +4,9 @@ public class UserAccountView {
     public void findUserAccountMenu(){
 
 
+
         System.out.println("" +
-                "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
         System.out.println(
                 "        ┏┓︱┏┓ ┏━━┓ ┏━━━┓\n" +
                 "        ┃┃︱┃┃ ┗┫┣┛ ┃┏━┓┃\n" +
@@ -13,8 +14,15 @@ public class UserAccountView {
                 "        ┃┏━┓┃ ︱┃┃︱ ┗━━┓┃\n" +
                 "        ┃┃︱┃┃ ┏┫┣┓ ┃┗━┛┃\n" +
                 "        ┗┛︱┗┛ ┗━━┛ ┗━━━┛");
+
         System.out.println(
-                "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+        System.out.println(""+
+                "┏━━━━━━━━━━━━━━━━━━━━━━┓\n" +
+                "┃           Hospital System V 1.0          ┃\n" +
+                "┗━━━━━━━━━━━━━━━━━━━━━━┛" +
+                "");
+        System.out.println();
         System.out.println("0\uFE0F⃣ 종료");
         System.out.println("1\uFE0F⃣ 로그인");
         System.out.println("2\uFE0F⃣ 회원가입");
